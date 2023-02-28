05093813b3c18903840c63e2f233ff148469de4a
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-02-28 04:20:00 +0000

    main logic in placed

--- 03_bmi_calculator/app/src/main/java/github/noargs/bmicalculator/MainActivity.kt
+++ 03_bmi_calculator/app/src/main/java/github/noargs/bmicalculator/MainActivity.kt
@@ -1,11 +1,91 @@
 package github.noargs.bmicalculator
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val weightText = findViewById<EditText>(R.id.etWeight)
+        val heightText = findViewById<EditText>(R.id.etHeight)
+        val calcButton = findViewById<Button>(R.id.btnCalculate)
+
+        calcButton.setOnClickListener {
+            val weight = weightText.text.toString()
+            val height = heightText.text.toString()
+
+            if(validateInput(weight, height)){
+                val bmi = weight.toFloat() / ((height.toFloat()/100)*(height.toFloat()/100))
+                val bmi2Digits = String.format("%.2f",bmi).toFloat()
+                displayResult(bmi2Digits)
+            }
+
+            hideKeyboard()
+        }
+    }
+
+    private fun validateInput(weight: String?, height: String?): Boolean {
+        when{
+            weight.isNullOrEmpty() -> {
+                Toast.makeText(this, "Weight field is empty", Toast.LENGTH_LONG).show()
+                return false
+            }
+            height.isNullOrEmpty() -> {
+                Toast.makeText(this, "Height field is empty", Toast.LENGTH_LONG).show()
+                return false
+            }
+            else -> {
+                return true
+            }
+        }
+    }
+
+    private fun displayResult(bmi: Float) {
+        val resultIndex = findViewById<TextView>(R.id.tvIndex)
+        val resultDesc = findViewById<TextView>(R.id.tvResult)
+        val info = findViewById<TextView>(R.id.tvInfo)
+
+        resultIndex.text = bmi.toString()
+        info.text = "(Normal range is 18.5 - 24.9)"
+
+        var resultText = ""
+        var color = 0
+
+        when{
+            bmi < 18.50 -> {
+                resultText = "Underweight"
+                color = R.color.underweight
+            }
+            bmi in 18.50..24.99 -> {
+                resultText = "Healthy"
+                color = R.color.normal
+            }
+            bmi in 25.00..29.99 -> {
+                resultText = "Overweight"
+                color = R.color.over_weight
+            }
+            bmi > 29.99 -> {
+                resultText = "Obese"
+                color = R.color.obese
+            }
+        }
+        resultDesc.setTextColor(ContextCompat.getColor(this,color))
+        resultDesc.text = resultText
+    }
+
+    private fun hideKeyboard(){
+        if (currentFocus != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        }
     }
 }
