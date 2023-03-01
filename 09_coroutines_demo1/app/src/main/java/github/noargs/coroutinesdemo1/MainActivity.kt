d1e482354ee362e22059f43ae2ffa8398b4b3877
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-03-01 17:51:32 +0000

    coroutine general and android specific dependency added to module gradle

--- 09_coroutines_demo1/app/src/main/java/github/noargs/coroutinesdemo1/MainActivity.kt
+++ 09_coroutines_demo1/app/src/main/java/github/noargs/coroutinesdemo1/MainActivity.kt
@@ -7,7 +7,9 @@ import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
