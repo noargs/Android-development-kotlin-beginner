cc463bae0482de111673961c0fe8707053e71cb1
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-03-01 04:26:43 +0000

    viewModel used to persist through configuration changes

---
+++ 06_view_model/app/src/main/java/github/noargs/viewmodel/MainActivityViewModel.kt
@@ -0,0 +1,12 @@
+package github.noargs.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel() {
+
+    var count = 0
+
+    fun updateCount(){
+        ++count
+    }
+}
